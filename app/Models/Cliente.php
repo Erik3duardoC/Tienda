2705df6bb996fdeca04c026bd86bd950283612d4
erik eduardo carmona ibarra <l1610661399@milpaalta2.tecnm.mx>
2023-12-12 17:13:34 -0600

    crud tienda

---
+++ app/Models/Cliente.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Cliente
+ *
+ * @property $id
+ * @property $nombre
+ * @property $email
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Producto[] $productos
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Cliente extends Model
+{
+    
+    static $rules = [
+		'nombre' => 'required',
+		'email' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','email'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function productos()
+    {
+        return $this->hasMany('App\Models\Producto', 'cliente_id', 'id');
+    }
+    
+
+}
